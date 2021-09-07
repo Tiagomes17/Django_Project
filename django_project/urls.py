eebac65d51eafb9b347b5504f7bd412074b94b36
unknown <rodrigo.cid@qloudyx.pt>
2021-09-07 15:55:03 +0100

    rerefe

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,4 @@
-# """django_poject URL Configuration
+# """django_project URL Configuration
 
 # The `urlpatterns` list routes URLs to views. For more information please see:
 #     https://docs.djangoproject.com/en/3.2/topics/http/urls/
