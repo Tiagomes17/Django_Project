af1dade56271895604b9d582ce55c842f6c69944
unknown <rodrigo.cid@qloudyx.pt>
2021-09-07 15:21:25 +0100

    Updated Settings

--- django_poject/settings.py
+++ django_poject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@ou6wu^&)2(b_jn331dpv!jdw0hz!p(wnxjt^*=xyk7ne_m&-'
-
+SECRET_KEY = os.environ.get('token_key')
+DEBUG_VALUE = os.environ.get('DEBUG_VALUE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xiagoqloudyxapp.heroku.com']
 
 
 # Application definition
